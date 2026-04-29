a66d4737f2e908f0efe5850b7dfd195ab664d0c0
rimskayama <rimskayama@outlook.com>
2026-04-29 22:49:42 +0300

    docs: added comment about shutdown hook as destructor analog

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
 
-        // обработчик завершения
+        // обработчик завершения - аналог деструктора
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             airport.shutdown();
         }));
